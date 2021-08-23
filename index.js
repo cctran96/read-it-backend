666f7a58f807f243260cdd62257b0cca9e0a1251
Chau Tran <cctran96@gmail.com>
2021-08-23 09:08:37 -0700

    add post model

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import mongoose from "mongoose"
 import express from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
+import postRoutes from "./routes/posts.js"
 
 dotenv.config()
 
 const app = express()
 const URL = process.env.URL
 const PORT = process.env.PORT || 5000
+
+app.use("/posts", postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
