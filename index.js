51e546d98de3a90a8956d86598b846737e9f6ca6
Chau Tran <cctran96@gmail.com>
2021-08-22 22:45:05 -0700

    database setup

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 import express from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
 
+dotenv.config()
+
+const app = express()
+const URL = process.env.URL
+const PORT = process.env.PORT || 5000
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch(error => console.log(error.message))
+
+mongoose.set("useFindAndModify", false)
