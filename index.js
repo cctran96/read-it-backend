e812c883424ceb078941e7fa3f337c65ecda7f25
Chau Tran <cctran96@gmail.com>
2021-08-24 20:29:16 -0700

    bug fix

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const app = express()
 const URL = process.env.URL
 const PORT = process.env.PORT || 5000
 
-app.use("/posts", postRoutes)
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+
+app.use("/posts", postRoutes)
 
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
