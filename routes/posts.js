9ca76bdbe74f707910869c0ed2a45c72fdb9b32d
Chau Tran <cctran96@gmail.com>
2021-09-10 19:32:53 -0700

    new route for user posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,14 @@
 import express from "express"
-import { getPosts, createPost, getPost, updatePost, deletePost, likePost } from "../controllers/posts.js"
 import auth from "../middleware/auth.js"
+import { 
+    getPosts, 
+    createPost, 
+    getPost, 
+    updatePost, 
+    deletePost, 
+    likePost, 
+    getUserPosts 
+} from "../controllers/posts.js"
 
 const router = express.Router()
 
@@ -10,5 +18,6 @@ router.get("/:id", getPost)
 router.patch(":/id", auth, updatePost)
 router.delete("/:id", auth, deletePost)
 router.patch("/:id/like", auth, likePost)
+router.get("/user/:id", getUserPosts)
 
 export default router
