aa79342c1324cafce5a1d4f02d5108484dbda971
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-09-17 21:33:19 -0700

    modified user model

--- models/User.js
+++ models/User.js
@@ -1,20 +1,11 @@
 import mongoose from "mongoose"
 
-const { ObjectId } = mongoose.Schema.Types
 
 const userSchema = mongoose.Schema({
     username: { type: String, required: true, minlength: 6, unique: true},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    bio: String,
-    posts: [{
-        type: ObjectId,
-        ref: "Post"
-    }],
-    communities: [{
-        type: ObjectId,
-        ref: "Community"
-    }]
+    bio: String
 })
 
 var User = mongoose.model("User", userSchema)
